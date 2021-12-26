4f330d04ae37db9e99d0c63a740cd19f56a6bd4f
jgrajewsky <jgrajewsky@gmail.com>
2021-12-26 12:20:20 +0100

    .

--- client/js/index.js
+++ client/js/index.js
@@ -20,16 +20,19 @@ function updatePage(href) {
         document.querySelector('[href="/' + path[1] + '"]').classList.add('nav-link-active');
     }
 
+    document.querySelector('#authors > h1, #authors > h2, #quotes > h1').style = path.length === 3 ? '' : 'display:none';
     if (path[1] === 'authors') {
         const authors = document.querySelector('#authors > .quotes');
         if (path.length === 2) {
             fetch(`/authors.json`).then(res => res.json()).then(data => {
-                authors.innerHTML = makeQuoteRows('authors', data);
+                for (const row of makeQuoteRows('authors', data)) {
+                    authors.append(row);
+                }
             });
         } else if (path.length === 3) {
             fetch(`/authors/${path[2]}.json`).then(res => res.json()).then(data => {
                 const name = path[2].replaceAll('-', ' ');
-                document.querySelector('#authors > h1').innerText = name;
+                document.querySelector('#authors > h1').innerText = `${name} Quotes`;
                 document.querySelector('#authors > h2').innerText = `Discover Most Famous Quotes by ${name}`;
                 authors.innerHTML = '';
                 for (const quote of data) {
@@ -37,17 +40,21 @@ function updatePage(href) {
                 }
             });
         } else {
+            authors.innerHTML = '';
             authors.append(makeQuote(path.slice(2).join('/'), true));
         }
     } else if (path[1] === 'quotes') {
         const quotes = document.querySelector('#quotes > .quotes');
         if (path.length === 2) {
             fetch(`/quotes.json`).then(res => res.json()).then(data => {
-                quotes.innerHTML = makeQuoteRows('quotes', data);
+                for (const row of makeQuoteRows('quotes', data)) {
+                    quotes.append(row);
+                }
             });
         } else {
             fetch(`/quotes/${path[2]}.json`).then(res => res.json()).then(data => {
                 document.querySelector('#quotes > h1').innerText = `${path[2].replaceAll('-', ' ')} Quotes`;
+                quotes.innerHTML = '';
                 for (const quote of data) {
                     quotes.append(makeQuote(quote, false));
                 }
@@ -56,30 +63,51 @@ function updatePage(href) {
     }
 }
 
+function onLink(link) {
+    link.onclick = () => {
+        if (link.href === window.location.href) {
+            return false;
+        }
+        history.pushState(null, '', link.href);
+        updatePage(link.getAttribute('href'));
+        return false;
+    };
+}
+
 function makeQuoteRows(prefix, data) {
-    let html = '';
+    let rows = [];
     for (const row of data) {
-        html += '<div class="content"><div><a>Magic Quotes</a> - Share our beautiful quote pictures on <a>Facebook</a>, <a>Twitter</a>, and <a>Pinterest</a></div><div>';
+        const element = document.createElement('div');
+        element.classList.add('quote-row');
+        const top = document.createElement('div');
+        top.innerHTML = `<a href="/${prefix}/${row}">${row.replaceAll('-', ' ')} Quotes</a> - Share our beautiful quote pictures on <a href="https://facebook.com/sharer.php?u=https://magicquotes.org/${prefix}/${row}">Facebook</a>, <a href="https://twitter.com/intent/tweet?url=https://magicquotes.org/${prefix}${row}">Twitter</a>, and <a href="https://pinterest.com/pin/create/button/?url=https://magicquotes.org/${prefix}/${row}">Pinterest</a>`;
+        element.append(top);
+        const bottom = document.createElement('div');
+        bottom.classList.add('quote-row-bottom');
         fetch(`/${prefix}/${row}.json`).then(res => res.json()).then(data => {
             for (const quote of data) {
-                html += `<div><img src="/authors/${row}/${quote}.jpeg"><div><a></a><a></a><a></a></div></div>`;
+                const quoteElement = document.createElement('div');
+                quoteElement.innerHTML = `<a href="/authors/${quote}"><img src="/authors/${quote}.jpeg"></a><div><a></a><a></a><a></a></div>`;
+                bottom.append(quoteElement);
             }
         });
-        html += '</div></div>';
+        element.append(bottom);
+        rows.push(element);
     }
-    return html;
+    return rows;
 }
 
 function makeQuote(name, big) {
     const quote = document.createElement('div');
     if (big) {
-        quote.classList.add('big-quote', 'content');
+        quote.classList.add('big-quote');
     }
     quote.classList.add('quote');
     const top = document.createElement(big ? 'blockquote' : 'a');
     top.classList.add('quote-top');
     if (!big) {
         top.href = `/authors/${name}`;
+        onLink(top);
     }
     top.innerHTML = `<img src="/authors/${name}.jpeg">`;
     const text = document.createTextNode('?');
@@ -89,6 +117,7 @@ function makeQuote(name, big) {
     author.classList.add('quote-author');
     author.href = `/authors/${name.split('/')[0]}`;
     author.innerText = '?';
+    onLink(author);
     quote.append(author);
     const tags = document.createElement('div');
     tags.innerText = 'Tags: ?';
@@ -98,7 +127,11 @@ function makeQuote(name, big) {
         author.innerText = data.author;
         tags.innerText = 'Tags:';
         for (const tag of data.tags) {
-            tags.innerHTML += `<a href="/quotes/${tag}">${tag} Quotes</a>`;
+            const tagElement = document.createElement('a');
+            tagElement.href = `/quotes/${tag}`;
+            tagElement.innerText = `${tag} Quotes`;
+            onLink(tagElement);
+            tags.append(tagElement);
         }
     });
     return quote;
@@ -109,24 +142,28 @@ function autorun() {
         updatePage(location.pathname);
     };
 
-    document.querySelectorAll('.nav-link').forEach(e => {
-        e.onclick = () => {
-            history.pushState(null, '', e.href);
-            updatePage(e.getAttribute('href'));
-            return false;
-        }
-    });
+    for (const link of document.querySelectorAll('.nav-link')) {
+        onLink(link);
+    }
 
     updatePage(location.pathname);
 
     fetch(`/authors.json`).then(res => res.json()).then(data => {
         for (const author of data) {
-            document.getElementById('author-list').innerHTML += `<a href="/authors/${author}">${author.replaceAll('-', ' ')}</a>`;
+            const link = document.createElement('a');
+            link.href = `/authors/${author}`;
+            link.innerText = author.replaceAll('-', ' ');
+            onLink(link);
+            document.getElementById('author-list').append(link);
         }
     });
     fetch(`/quotes.json`).then(res => res.json()).then(data => {
         for (const tag of data) {
-            document.getElementById('quote-list').innerHTML += `<a href="/quotes/${tag}">${tag} Quotes</a>`;
+            const link = document.createElement('a');
+            link.href = `/quotes/${tag}`;
+            link.innerText = `${tag} Quotes`;
+            onLink(link);
+            document.getElementById('quote-list').append(link);
         }
     });
 }
